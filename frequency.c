2c75bac440d39b12173daa425d466f863dc6c926
ynnmuraii <tihookeanski.y.tanruuu@gmail.com>
2024-10-09 20:45:28 +0400

    did a frequency check for standart C

--- frequency.c
+++ frequency.c
@@ -8,4 +8,16 @@ int main() {
     printf("Enter something: ");
     fgets(str, 256, stdin);  
 
+    for (int i = 0; i < strlen(str); i++) {
+        unsigned char ch = str[i];
+        freq[ch]++;
+    }
+
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] != 0) {
+            printf("The character '%c' occurs %d times\n", i, freq[i]);
+        }
+    }
+
+    return 0;
 }
